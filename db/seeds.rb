13c8b203f47b8d2164ef8eccd8e53074aa053d86
nguyenduong2612 <nguyenduong261298@gmail.com>
2019-12-23 01:16:35 +0700

    update layouts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name:  "Admin",
     activated: true,
     activated_at: Time.zone.now)
 
-99.times do |n|
+10.times do |n|
 name  = Faker::Name.name
 email = "example-#{n+1}@gmail.com"
 password = "123456"
